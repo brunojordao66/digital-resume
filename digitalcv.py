b64f75281f64491b6f294be9836a193274aa0bc4
brunojordao66 <62249609+brunojordao66@users.noreply.github.com>
2024-08-14 18:31:00 -0300

    Update digitalcv.py

--- digitalcv.py
+++ digitalcv.py
@@ -3,10 +3,10 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image
 
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file =  current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "resumee en.pdf"
-profile_pic = current_dir / "assets" / 'profilepic.PNG'
+##current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+##css_file =  current_dir / "styles" / "main.css"
+##resume_file = current_dir / "assets" / "resumee en.pdf"
+##profile_pic = current_dir / "assets" / 'profilepic.PNG'
 
 
 ## informaçoes gerais
@@ -46,13 +46,13 @@ with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()),unsafe_allow_html=True)
 with open(resume_file,'rb') as pdf_file:
     PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
+##profile_pic = Image.open(profile_pic)
 
 ## formatando apresentação
 
 col1, col2 = st.columns(2, gap='small')
 with col1:
-    st.image(profile_pic, width=250)
+    ##st.image(profile_pic, width=250)
 with col2:
     st.title(name)
     st.subheader(personal_info)
